8784ba6c7942802bf6b0344f138146293fff2c84
Jorge Pulido <jorgepulido@Mac-mini-Alfredo-Kubik.local>
2022-01-19 09:49:10 -0600

    build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f0837a4174af7d96ea5a";
+/******/ 	var hotCurrentHash = "8d6752db291c4fe896e9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
